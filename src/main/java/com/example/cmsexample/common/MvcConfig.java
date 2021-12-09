5777925d575ecec36d2784927ad269e6e2fc1945
ILoveMemes <degtyarev_is@mail.ru>
2021-12-09 14:08:05 +0300

    added ETag support

--- src/main/java/com/example/cmsexample/common/MvcConfig.java
+++ src/main/java/com/example/cmsexample/common/MvcConfig.java
@@ -1,14 +1,23 @@
 package com.example.cmsexample.common;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Bean
+    public Filter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
     }
 
 }
